903ba904fa3b97f2fd0813c0fc4ba0fc1d4c6673
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-01-26 18:49:17 -0500

    Take optional center point for polar coordinate (#31)

--- src/main/java/frc/robot/coordinates/PolarCoordinate.java
+++ src/main/java/frc/robot/coordinates/PolarCoordinate.java
@@ -2,17 +2,40 @@ package frc.robot.coordinates;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 import frc.robot.Constants;
 
 public class PolarCoordinate {
 
   private double radiusMeters;
   private Rotation2d theta;
+  // The reference point is the "center" in our polar coordinate system
+  private Translation2d referencePoint;
 
+  /**
+   * Construct a polar coordinate using the center of the field (the Hub)
+   * as the reference point.
+   *
+   * @param radiusMeters - The radius from the reference point in meters.
+   * @param theta        - The rotation from the reference point. In our
+   *                     field-centric system, this is the left-hand side.
+   */
   public PolarCoordinate(double radiusMeters, Rotation2d theta) {
+    this(radiusMeters, theta, Constants.kHub);
+  }
+
+  /**
+   * Construct a polar coordinate using a specific center point.
+   *
+   * @param radiusMeters   - The radius from the reference point in meters.
+   * @param theta          - The rotation from the reference point. In our
+   *                       field-centric system, this is the left-hand side.
+   * @param referencePoint - The x, y to use as the center for our polar
+   *                       coordinate. Should be a field coordinate.
+   */
+  public PolarCoordinate(double radiusMeters, Rotation2d theta, Translation2d referencePoint) {
     this.radiusMeters = radiusMeters;
     this.theta = theta;
+    this.referencePoint = referencePoint;
   }
 
   public double getRadiusMeters() {
@@ -23,35 +46,36 @@ public class PolarCoordinate {
     return theta;
   }
 
+  public Translation2d getReferencePoint() {
+    return referencePoint;
+  }
+
   /**
    * Translates our polar coordinate to a x, y in a cartestian
-   * coordinate system relative to the center of the field (the Hub).
-   * 
+   * coordinate system relative to our reference point on the field.
+   *
    * @return A x, y of our polar coordinate represented in a cartesian coordinate
-   *         system relative to the center of the field.
+   *         system relative to the reference point.
    */
   public Translation2d toFieldCoordinate() {
-    // Hub is in the center of the field
-    // Center of field is (27ft, 13.5ft)
-    Translation2d fieldCenter = Constants.kHub;
     Translation2d translation = this.toCartesianCoordinate();
     /**
      * We need to add our CC's X (width) to our field Y (width)
      * and our CC's Y (height) to our field X (height).
      */
     return new Translation2d(
-      fieldCenter.getX() + translation.getY(),
-      fieldCenter.getY() + translation.getX()
+      referencePoint.getX() + translation.getY(),
+      referencePoint.getY() + translation.getX()
     );
   }
 
   /**
    * Translates our polar coordinate to a cartesian coordinate
-   * 
+   *
    * @return A x, y of our polar coordinate represented in a cartesian coordinate
    *         system relative.
    */
-  public Translation2d toCartesianCoordinate() {
+  private Translation2d toCartesianCoordinate() {
     double x = radiusMeters * Math.cos(theta.getRadians());
     double y = radiusMeters * Math.sin(theta.getRadians());
     return new Translation2d(x, y);
