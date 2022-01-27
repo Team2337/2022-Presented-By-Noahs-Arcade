51cd66e6aceeea96ef199b53cfdf61c36e05734a
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-01-27 18:56:11 -0500

    Attempt to use PolarCoordinate.fromFieldCoordinate for our look towards point

--- src/main/java/frc/robot/commands/OdometryHeadingCommand.java
+++ src/main/java/frc/robot/commands/OdometryHeadingCommand.java
@@ -1,10 +1,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
+import frc.robot.coordinates.PolarCoordinate;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Heading;
 
@@ -31,6 +31,7 @@ public class OdometryHeadingCommand extends CommandBase {
     addRequirements(heading);
   }
 
+  /*
   @Override
   public void execute() {
     Pose2d pose = drivetrain.getPose();
@@ -40,6 +41,19 @@ public class OdometryHeadingCommand extends CommandBase {
     // The angle is the angle outward from our center point. In order to face our center
     // point, we need to rotate our angle by 180 degrees.
     heading.setMaintainHeading(new Rotation2d(angle).unaryMinus());
+  }
+  */
+
+  @Override
+  public void execute() {
+    Pose2d pose = drivetrain.getPose();
+    PolarCoordinate polarCoordinate = PolarCoordinate.fromFieldCoordinate(
+      pose.getTranslation(),
+      point
+    );
+    // The angle is the angle outward from our center point. In order to face 
+    // our center point, we need to rotate our angle by 180 degrees.
+    heading.setMaintainHeading(polarCoordinate.getTheta().unaryMinus());
   }
 
 }
