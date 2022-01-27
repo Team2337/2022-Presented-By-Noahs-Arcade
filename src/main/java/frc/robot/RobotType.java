faa034740e545d1b296452cfcb0b1716943eaa58
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-01-26 23:12:42 -0500

    Add class + types for getting the Robot type (practice or comp) (#23)
    * Add class + types for getting the Robot type (practice or comp)
    
    * Added Practice bot mac address
    
    Co-authored-by: slynch05 <slynch05@yahoo.com>

---
+++ src/main/java/frc/robot/RobotType.java
@@ -0,0 +1,39 @@
+package frc.robot;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class RobotType {
+
+  public enum Type {
+    COMPETITION,
+    PRACTICE
+  }
+
+  private static String kPracticeBotMAC = "00:80:2F:17:89:85";
+
+  public static Type getRobotType() {
+    String mac = getMACAddress();
+    if (mac != null && mac.equals(kPracticeBotMAC)) {
+      return Type.PRACTICE;
+    }
+    return Type.COMPETITION;
+  }
+
+  private static String getMACAddress() {
+    try {
+      NetworkInterface network = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+      byte[] address = network.getHardwareAddress();
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < address.length; i++) {
+        sb.append(String.format("%02X%s", address[i], (i < address.length - 1) ? ":" : ""));
+      }
+      return sb.toString();
+    } catch (UnknownHostException | SocketException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+}
