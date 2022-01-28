0be65a8a8de2a88bbc78760fec1f85f31aff7bee
slynch05 <slynch05@yahoo.com>
2022-01-28 11:46:36 -0500

    Added MK4i_L1 configuration

--- src/main/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurations.java
+++ src/main/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurations.java
@@ -23,6 +23,13 @@ public final class SdsModuleConfigurations {
             (15.0 / 32.0) * (10.0 / 60.0),
             true
     );
+    public static final ModuleConfiguration MK4i_L1 = new ModuleConfiguration(
+            0.10033,
+            (14.0 / 50.0) * (25.0 / 19.0) * (15.0 / 45.0),
+            true,
+            (14.0 / 50.0) * (10.0 / 60.0),
+            true
+    );
     public static final ModuleConfiguration MK4_L2 = new ModuleConfiguration(
             0.10033,
             (14.0 / 50.0) * (27.0 / 17.0) * (15.0 / 45.0),
