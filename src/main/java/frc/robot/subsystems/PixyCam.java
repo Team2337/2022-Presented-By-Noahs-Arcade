320df4f61af00e414dce76b14c6a739c3261f90c
M. Francis <mfrancis48439@gmail.com>
2022-02-02 19:36:18 -0500

    Attempted to fix potential memory leak
    From testing, memory leak appears to happen when there's too many Blocks being created at one time.
    
    - Got rid of some variables that weren't necessary
    - Changed the return types of some things
    - Got rid of a redundant check if a list was empty, which should (I say should since this code is untested) fix a bug where the code said that it still saw a ball when a ball was removed from view
    - Removed a bit of code from testing in the filter function

--- src/main/java/frc/robot/subsystems/PixyCam.java
+++ src/main/java/frc/robot/subsystems/PixyCam.java
@@ -22,9 +22,7 @@ public class PixyCam extends SubsystemBase {
   private int state;
   private boolean connected;
 
-  private int numberOfTargets = 0;
-  private ArrayList<Block> blocks = new ArrayList<Block>();
-  private ArrayList<Block> filteredBlocks = new ArrayList<Block>();
+  // private ArrayList<Block> blocks = new ArrayList<Block>();
 
   private Block redTarget;
   private Block blueTarget;
@@ -59,8 +57,8 @@ public class PixyCam extends SubsystemBase {
 
     // Info widget
     ShuffleboardLayout infoWidget = pixyTab.getLayout("Vision Info", BuiltInLayouts.kList).withSize(8, 6).withPosition(4, 4);
-    infoWidget.addNumber("Number of Blocks", () -> blocks.size());
-    infoWidget.addNumber("Number of Targets", () -> filteredBlocks.size());
+    // infoWidget.addNumber("Number of Blocks", () -> blocks.size());
+    // infoWidget.addNumber("Number of Targets", () -> filteredBlocks.size());
     infoWidget.addNumber("Red target x", () -> {
       return redTarget == null ? -1 : redTarget.getX();
     });
@@ -81,59 +79,52 @@ public class PixyCam extends SubsystemBase {
     });
     infoWidget.addNumber("Pixy State", () -> state);
 
-    pixyTab.addBoolean("Sees Target", () -> (numberOfTargets > 0))
-      .withSize(4, 4)
-      .withPosition(4, 0);
+    // pixyTab.addBoolean("Sees Target", () -> (blocks.size() > 0))
+    //   .withSize(4, 4)
+    //   .withPosition(4, 0);
   }
 
   @Override
   public void periodic() {
-    // Update Pixy values
-    updatePixy();
-
     // Filter target values
-    filterTargets();
+    filterTargets(updatePixy());
   }
 
   /**
    * Updates the values of the PixyCam
    */
-  private void updatePixy() {
+  private ArrayList<Block> updatePixy() {
     // Check to see if the camera initialized correctly.
     if(!connected) {
       // Attempt to reconnect Pixy
       state = pixycam.init(chip);
       connected = (state >= 0);
-      if(!connected) return;
+      if(!connected) return null;
     }
 
     // Either number of targets or an error code
-    int error = pixycam.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG_ALL, 48);
+    int error = pixycam.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG_ALL, 20);
 
     // If there was an error (i.e. Pixy is busy getting new values), don't update variables
     if(error < 0)
-      return;
+      return null;
 
-    numberOfTargets = error;
-    blocks = pixycam.getCCC().getBlockCache();
+    return pixycam.getCCC().getBlockCache();
   }
 
   /**
    * Filters the targets based on conditions that make them seem "cargo-like"
    */
-  private void filterTargets() {
-    // Skip the method if error value
-    if(numberOfTargets < 0)
+  private void filterTargets(ArrayList<Block> blocks) {
+    if (blocks == null) {
       return;
+    }
     
-    // Clear last entries
+    // Update entries
     Block bestRedBlock = null;
     Block bestBlueBlock = null;
-    filteredBlocks.clear();
 
-    // Skip the filtering if there are no targets
-    if(numberOfTargets == 0)
-      return;
+    ArrayList<Block> filteredBlocks = new ArrayList<Block>();
 
     for(Block block : blocks) {
       // Get ratio of width to height
@@ -151,21 +142,16 @@ public class PixyCam extends SubsystemBase {
 
         // Set red or blue target to whatever this one is
         double area = block.getWidth() * block.getHeight();
-        if(block.getSignature() == 1){
-          if(redTarget == null || redTarget.getWidth() * redTarget.getHeight() < area){
+
+        if (block.getSignature() == 1) {
+          if (redTarget == null || redTarget.getWidth() * redTarget.getHeight() < area) {
             bestRedBlock = block;
           }
         } else {
-          if(blueTarget == null || blueTarget.getWidth() * blueTarget.getHeight() < area){
+          if (blueTarget == null || blueTarget.getWidth() * blueTarget.getHeight() < area) {
             bestBlueBlock = block;
           }
         }
-      }
-      // TODO: testing, remove after
-      if(block.getSignature() == 1){
-        bestRedBlock = block;
-      } else {
-        bestBlueBlock = block;
       }
     }
 
@@ -214,16 +200,20 @@ public class PixyCam extends SubsystemBase {
   /**
    * @param target The target {@link Block}
    * @return The target converted to an angle from center of Pixy.
-   * Ranges from -30 to 30.
+   * Ranges from -30 to 30. Returns empty if target is null.
    */
-  public double getTargetAngle(Block target){
+  public Optional<Double> getTargetAngle(Block target) {
+    if (target == null){
+      return Optional.empty();
+    }
+
     /**
      * To get the angle, we divide the x (which ranges from 0 to 315, the width
      * of the camera) by 315 to get it as a percentage from 0-1. We multiply
      * that by 60 (the field of view of the PixyCam) to get it in terms of
      * degrees, and then subtract it by 30 to center it.
      */
-    return ((target.getX() / 315.0) * 60.0) - 30.0;
+    return Optional.of(((target.getX() / 315.0) * 60.0) - 30.0);
   }
 
 }
