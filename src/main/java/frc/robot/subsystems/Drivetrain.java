80699026845ac7510cdef56eb9a2cd35d0bc3e05
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-02-05 11:05:21 -0500

    Constants for Practice/Skills/Comp Bot (#37)
    * Set clockwise for 2022 practice bot
    
    * Changed offset values
    
    * Implemented RobotType
    
    * Added MK4i_L1 configuration
    
    * Updated MK4swerve configuration to include options for MK4i with new gear reduction
    
    Added a skillsbot designation for RobotType
    Updated Drivetrain to switch between MK4 and MK4i modules.
    
    * Added smartdashboard object for mac address.
    
    * Added mac addresses for different robots
    
    * Remove SDS code from robot constants branch
    
    * Added case statement for motor ids
    
    * Formatting cleanup on constants
    
    * Combine case statements for practice and comp
    
    * Clean up stray SmartDashboard print
    
    Co-authored-by: madisonjohnston <madison.johnston333@gmail.com>
    Co-authored-by: Sean Lynch <slynch05@yahoo.com>

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -22,6 +22,8 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.RobotType;
+import frc.robot.RobotType.Type;
 
 public class Drivetrain extends SubsystemBase {
 
@@ -93,41 +95,41 @@ public class Drivetrain extends SubsystemBase {
         tab.getLayout("Front Right Module", BuiltInLayouts.kList)
           .withSize(4, 8)
           .withPosition(4, 0),
-        Mk4SwerveModuleHelper.GearRatio.L1,
-        Constants.MODULE0_DRIVE_MOTOR_ID,
-        Constants.MODULE0_ANGLE_MOTOR_ID,
-        Constants.MODULE0_ANGLE_CANCODER_ID,
-        Constants.MODULE0_ANGLE_OFFSET
+        RobotType.getRobotType() == Type.SKILLSBOT ? Mk4SwerveModuleHelper.GearRatio.L1 : Mk4SwerveModuleHelper.GearRatio.L1I,
+        Constants.getInstance().MODULE0_DRIVE_MOTOR_ID,
+        Constants.getInstance().MODULE0_ANGLE_MOTOR_ID,
+        Constants.getInstance().MODULE0_ANGLE_CANCODER_ID,
+        Constants.getInstance().MODULE0_ANGLE_OFFSET
       ),
       Mk4SwerveModuleHelper.createFalcon500(
         tab.getLayout("Front Left Module", BuiltInLayouts.kList)
           .withSize(4, 8)
           .withPosition(0, 0),
-        Mk4SwerveModuleHelper.GearRatio.L1,
-        Constants.MODULE1_DRIVE_MOTOR_ID,
-        Constants.MODULE1_ANGLE_MOTOR_ID,
-        Constants.MODULE1_ANGLE_CANCODER_ID,
-        Constants.MODULE1_ANGLE_OFFSET
+        RobotType.getRobotType() == Type.SKILLSBOT ? Mk4SwerveModuleHelper.GearRatio.L1 : Mk4SwerveModuleHelper.GearRatio.L1I,
+        Constants.getInstance().MODULE1_DRIVE_MOTOR_ID,
+        Constants.getInstance().MODULE1_ANGLE_MOTOR_ID,
+        Constants.getInstance().MODULE1_ANGLE_CANCODER_ID,
+        Constants.getInstance().MODULE1_ANGLE_OFFSET
       ),
       Mk4SwerveModuleHelper.createFalcon500(
         tab.getLayout("Back Left Module", BuiltInLayouts.kList)
           .withSize(4, 8)
           .withPosition(0, 8),
-        Mk4SwerveModuleHelper.GearRatio.L1,
-        Constants.MODULE2_DRIVE_MOTOR_ID,
-        Constants.MODULE2_ANGLE_MOTOR_ID,
-        Constants.MODULE2_ANGLE_CANCODER_ID,
-        Constants.MODULE2_ANGLE_OFFSET
+        RobotType.getRobotType() == Type.SKILLSBOT ? Mk4SwerveModuleHelper.GearRatio.L1 : Mk4SwerveModuleHelper.GearRatio.L1I,
+        Constants.getInstance().MODULE2_DRIVE_MOTOR_ID,
+        Constants.getInstance().MODULE2_ANGLE_MOTOR_ID,
+        Constants.getInstance().MODULE2_ANGLE_CANCODER_ID,
+        Constants.getInstance().MODULE2_ANGLE_OFFSET
       ),
       Mk4SwerveModuleHelper.createFalcon500(
         tab.getLayout("Back Right Module", BuiltInLayouts.kList)
           .withSize(4, 8)
           .withPosition(4, 8),
-        Mk4SwerveModuleHelper.GearRatio.L1,
-        Constants.MODULE3_DRIVE_MOTOR_ID,
-        Constants.MODULE3_ANGLE_MOTOR_ID,
-        Constants.MODULE3_ANGLE_CANCODER_ID,
-        Constants.MODULE3_ANGLE_OFFSET
+        RobotType.getRobotType() == Type.SKILLSBOT ? Mk4SwerveModuleHelper.GearRatio.L1 : Mk4SwerveModuleHelper.GearRatio.L1I,
+        Constants.getInstance().MODULE3_DRIVE_MOTOR_ID,
+        Constants.getInstance().MODULE3_ANGLE_MOTOR_ID,
+        Constants.getInstance().MODULE3_ANGLE_CANCODER_ID,
+        Constants.getInstance().MODULE3_ANGLE_OFFSET
       )
     };
 
