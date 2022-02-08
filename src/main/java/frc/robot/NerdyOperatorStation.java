f9d8532b9eb04b84d237e86410491f1c79bb8700
M. Francis <mfrancis48439@gmail.com>
2022-02-07 20:47:11 -0500

    Naming changes
    Variables now have proper naming and method names are more descriptive

--- src/main/java/frc/robot/NerdyOperatorStation.java
+++ src/main/java/frc/robot/NerdyOperatorStation.java
@@ -22,31 +22,31 @@ public class NerdyOperatorStation extends Joystick {
     * driver's station is redesigned.
     *
     */
-  public JoystickButton  GreenButton  = new JoystickButton(this, 1);
-  public JoystickButton  RedButton    = new JoystickButton(this, 2);
-  public JoystickButton  WhiteButton  = new JoystickButton(this, 7);
-  public JoystickButton  YellowButton = new JoystickButton(this, 8);
-  public JoystickButton  BlueButton   = new JoystickButton(this, 9);
-  public JoystickButton  BlackButton  = new JoystickButton(this, 10);
+  public JoystickButton  greenButton  = new JoystickButton(this, 1);
+  public JoystickButton  redButton    = new JoystickButton(this, 2);
+  public JoystickButton  whiteButton  = new JoystickButton(this, 7);
+  public JoystickButton  yellowButton = new JoystickButton(this, 8);
+  public JoystickButton  blueButton   = new JoystickButton(this, 9);
+  public JoystickButton  blackButton  = new JoystickButton(this, 10);
 
-  public JoystickButton  ClearSwitch  = new JoystickButton(this, 3);
-  public JoystickButton  YellowSwitch = new JoystickButton(this, 4);
-  public JoystickButton  BlueSwitch   = new JoystickButton(this, 5);
-  public JoystickButton  BlackSwitch  = new JoystickButton(this, 6);
+  public JoystickButton  clearSwitch  = new JoystickButton(this, 3);
+  public JoystickButton  yellowSwitch = new JoystickButton(this, 4);
+  public JoystickButton  blueSwitch   = new JoystickButton(this, 5);
+  public JoystickButton  blackSwitch  = new JoystickButton(this, 6);
 
-  public boolean clearSwitchOn() {
-    return this.ClearSwitch.get();
+  public boolean isClearSwitchOn() {
+    return this.clearSwitch.get();
   }
 
-  public boolean yellowSwitchOn() {
-    return this.YellowSwitch.get();
+  public boolean isYellowSwitchOn() {
+    return this.yellowSwitch.get();
   }
 
-  public boolean blackSwitchOn() {
-    return this.BlackSwitch.get();
+  public boolean isBlackSwitchOn() {
+    return this.blackSwitch.get();
   }
 
-  public boolean blueSwitchOn() {
-    return this.BlueSwitch.get();
+  public boolean isBlueSwitchOn() {
+    return this.blueSwitch.get();
   }
 }
