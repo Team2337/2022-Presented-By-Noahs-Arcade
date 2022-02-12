122e35105250dabeb3bcbc32bd4297b3dd0a076e
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-02-12 15:31:37 -0500

    Add default comment

--- src/main/java/frc/robot/CTREUtils.java
+++ src/main/java/frc/robot/CTREUtils.java
@@ -5,9 +5,9 @@ import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 public class CTREUtils {
 
   /**
-   * Creates a current limit configuration with some generic values.
-   * 
-   * @return The generic current limit configuration
+   * Creates a default limit configuration. This is base the current limit
+   * configuration we use on our motors to prevent overheating the motors
+   * in catastrophic scenerios.
    */
   public static StatorCurrentLimitConfiguration defaultCurrentLimit() {
     return new StatorCurrentLimitConfiguration(true, 50.0, 40.0, 3.0);
