ef04dde2d884347e394fb8fb3256e4dc67019ae4
M. Francis <58670911+mfrancis33@users.noreply.github.com>
2022-02-12 11:08:54 -0500

    Update to intake: beam break sensor (#65)
    * Moved intake to this branch
    
    * Remove second motor and DIO Constants class
    
    Turns out, thanks to a misunderstanding, we didn't need the second motor! Woo! I removed that. Also, I added a class in the Constants file for DIO ports (of which there should be 4 total, but only 1 here).
    
    One more change, intake speeds don't need to be converted from Celsius to Fahrenheit. That's for temperature. Not sure how I made that mistake.
    
    * Changed motor id constant names
    
    Forgot to change the motor id constants. This has been fixed.
    
    * Remove DIO class in Constants and some Javadocs
    
    Removed DIO class in constants for consistency purposes. Also added Javadocs for stopIntake and setIntakeSpeed. Moved setIntakeSpeed in the file and made it private.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -243,6 +243,8 @@ public final class Constants {
       Math.hypot(Units.inchesToMeters(Constants.getInstance().DRIVETRAIN_TRACK_WIDTH_INCHES) / 2.0, Units.inchesToMeters(Constants.getInstance().DRIVETRAIN_WHEEL_BASE_INCHES) / 2.0);
   }
 
+  public static final int INTAKE_SENSOR_ID = 0;
+
   public static final int INTAKE_MOTOR_ID = 6;
   public static final double INTAKE_SPEED = 0.5;
 
