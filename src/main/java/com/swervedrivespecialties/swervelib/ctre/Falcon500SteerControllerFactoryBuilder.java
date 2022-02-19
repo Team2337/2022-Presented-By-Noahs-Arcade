2a1d5ffc837f4316e481a62b21f4fc61d840445e
Sean Lynch <slynch05@yahoo.com>
2022-02-19 00:36:57 -0500

    Attempted fix for module resets.

--- src/main/java/com/swervedrivespecialties/swervelib/ctre/Falcon500SteerControllerFactoryBuilder.java
+++ src/main/java/com/swervedrivespecialties/swervelib/ctre/Falcon500SteerControllerFactoryBuilder.java
@@ -184,16 +184,21 @@ public final class Falcon500SteerControllerFactoryBuilder {
         @Override
         public void setReferenceAngle(double referenceAngleRadians) {
             double currentAngleRadians = motor.getSelectedSensorPosition() * motorEncoderPositionCoefficient;
-
+            
+            
             // Reset the NEO's encoder periodically when the module is not rotating.
             // Sometimes (~5% of the time) when we initialize, the absolute encoder isn't fully set up, and we don't
             // end up getting a good reading. If we reset periodically this won't matter anymore.
             if (motor.getSelectedSensorVelocity() * motorEncoderVelocityCoefficient < ENCODER_RESET_MAX_ANGULAR_VELOCITY) {
                 if (++resetIteration >= ENCODER_RESET_ITERATIONS) {
-                    resetIteration = 0;
                     double absoluteAngle = absoluteEncoder.getAbsoluteAngle();
-                    motor.setSelectedSensorPosition(absoluteAngle / motorEncoderPositionCoefficient);
-                    currentAngleRadians = absoluteAngle;
+                    if (absoluteEncoder.getError().value != 0) {
+                        resetIteration = ENCODER_RESET_ITERATIONS - 1;
+                    } else {
+                        motor.setSelectedSensorPosition(absoluteAngle / motorEncoderPositionCoefficient);
+                        currentAngleRadians = absoluteAngle;
+                        resetIteration = 0;
+                    }
                 }
             } else {
                 resetIteration = 0;
