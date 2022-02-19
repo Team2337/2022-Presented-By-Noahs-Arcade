76421fe4c32ae410f56c9fc78664f54ecaadebee
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-02-18 21:53:10 -0500

    Use drivetrain::getTranslation syntax for HeadingToTargetCommand

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -41,7 +41,7 @@ public class RobotContainer {
 
   public RobotContainer() {
     drivetrain.setDefaultCommand(new SwerveDriveCommand(driverController, autoDrive, heading, drivetrain));
-    heading.setDefaultCommand(new HeadingToTargetCommand(() -> drivetrain.getPose().getTranslation(), heading));
+    heading.setDefaultCommand(new HeadingToTargetCommand(drivetrain::getTranslation, heading));
 
     // Configure the button bindings
     configureButtonBindings();
