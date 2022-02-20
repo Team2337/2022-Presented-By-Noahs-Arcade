af9eff6068d1943f6bb26e8781f4b672186e8a62
Nicholas-Stokes <nicholasbstokes56@gbstu.org>
2022-02-19 20:17:24 -0500

    Tracks how long for the motor to reach setpoint

--- src/main/java/frc/robot/commands/shooter/StartShooter.java
+++ src/main/java/frc/robot/commands/shooter/StartShooter.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.shooter;
 
 import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
@@ -11,6 +12,10 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class StartShooter extends CommandBase {
 
   private final Shooter shooter;
+  private double reachSetpointTime;
+  private boolean start = true;
+  private double previousSetpoint;
+  private double newSetpoint;
 
   public StartShooter(Shooter shooter) {
     this.shooter = shooter;
@@ -19,8 +24,24 @@ public class StartShooter extends CommandBase {
   }
 
   @Override
+  public void initialize(){
+    reachSetpointTime = 0;
+    start = true;
+  }
+  @Override
   public void execute() {
+    newSetpoint = shooter.shooterSpeedFeetPerSecondWidget.getDouble(0);
     shooter.setSpeed(shooter.shooterSpeedFeetPerSecondWidget.getDouble(0));
+    if (start || (previousSetpoint != newSetpoint)) {
+      if (!shooter.isShooterToSpeed()) {
+        reachSetpointTime ++;
+      }
+      else {
+      start = false;
+      SmartDashboard.putNumber("Time to reach Setpoint in sec", ((reachSetpointTime * 50) / 1000));
+      }
+    }
+    previousSetpoint = shooter.shooterSpeedFeetPerSecondWidget.getDouble(0);
   }
 
   @Override
