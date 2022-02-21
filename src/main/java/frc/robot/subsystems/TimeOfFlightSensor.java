592b386be65e6516c0fc965bbd6f2a0277a847d3
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-02-20 23:07:41 -0500

    Improve update distance flow

--- src/main/java/frc/robot/subsystems/TimeOfFlightSensor.java
+++ src/main/java/frc/robot/subsystems/TimeOfFlightSensor.java
@@ -8,36 +8,37 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TimeOfFlightSensor extends SubsystemBase {
-  
+
   private final TimeOfFlight sensor = new TimeOfFlight(0);
-  private double previousDistance = 0.0;
 
-  public TimeOfFlightSensor() {
-
-  }
+  private double distanceInches;
 
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Distance (in)", getDistanceInches());
-    SmartDashboard.putString("Status", getStatus().toString());
+    if (getStatus() == Status.Valid) {
+      // mm -> m, then m -> inches
+      distanceInches = Units.metersToInches((getDistanceMM() / 1000));
+    }
+
+    SmartDashboard.putNumber("TimeOfFlight/Distance (in)", getDistanceInches());
+    SmartDashboard.putString("TimeOfFlight/Status", getStatus().toString());
   }
 
-  public Status getStatus() {
+  private Status getStatus() {
     return sensor.getStatus();
   }
 
-  public double getDistanceMM() {
+  private double getDistanceMM() {
     return sensor.getRange();
   }
-  
+
+  /**
+   * Get the last read distance from the sensor in inches.
+   * Note: If the sensor's status is no longer valid, we will
+   * remember it's last returned distance.
+   */
   public double getDistanceInches() {
-    if (getStatus() == Status.Valid){
-      double distance = Units.metersToInches((getDistanceMM() / 1000));
-      previousDistance = distance;
-      return distance;
-    }
-    else {
-      return previousDistance;
-    }
+    return distanceInches;
   }
+
 }
