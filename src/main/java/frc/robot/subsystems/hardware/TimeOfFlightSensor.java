ef864ad266c6ed582d3d62acf104ce5b9b0d8710
Nicholas-Stokes <nicholasbstokes56@gbstu.org>
2022-02-21 14:27:48 -0500

    Added counter for Invalids
    Cycles subject to change. If over threshold for invalids (or any other error code) in a row, we get a -1.

--- src/main/java/frc/robot/subsystems/hardware/TimeOfFlightSensor.java
+++ src/main/java/frc/robot/subsystems/hardware/TimeOfFlightSensor.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.hardware;
 
 import com.playingwithfusion.TimeOfFlight;
 import com.playingwithfusion.TimeOfFlight.Status;
-
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,21 +9,40 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class TimeOfFlightSensor extends SubsystemBase {
 
   private final TimeOfFlight sensor = new TimeOfFlight(0);
-
+  
   private double distanceInches;
+  private int validCounter = 0;
+
+  private final int MAX_VALID_COUNTER = 3;
+
+/*Hardware Failure
+Hardware failure
+InternalError	
+Internal algorithm underflow or overflow
+Invalid	
+The measured distance is invalid
+ReturnPhaseBad	
+Return signal phase is out of bounds.
+ReturnSignalLow	
+Return signal value is below the internal defined threshold.
+SigmaHigh	
+Sigma estimator check is above internally defined threshold.
+Valid	
+Measured distance is valid
+WrappedTarget	
+Wrapped target, non-matching phases.*/
 
   @Override
   public void periodic() {
-  if (getStatus() == Status.Valid || getStatus() == Status.Invalid){
     if (getStatus() == Status.Valid) {
+      validCounter = 0;
       // mm -> m, then m -> inches
       distanceInches = Units.metersToInches((getDistanceMM() / 1000));
+    } else if (validCounter >= MAX_VALID_COUNTER) {
+      distanceInches = -1;
     }
-  }
-  else {
-    distanceInches = -1; //-1 can be our error code for if our sensor gets messed up
-  }
 
+    validCounter++;
 
     SmartDashboard.putNumber("TimeOfFlight/Distance (in)", getDistanceInches());
     SmartDashboard.putString("TimeOfFlight/Status", getStatus().toString());
