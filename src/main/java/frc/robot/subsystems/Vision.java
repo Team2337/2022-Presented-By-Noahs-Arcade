d21389398cbc3c8aff39d5605b8714810cadf190
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-02-23 17:23:02 -0500

    Remove unused imports from Vision subsystem

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,30 +1,13 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.filter.MedianFilter;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.RobotType;
-import frc.robot.coordinates.PolarCoordinate;
 import frc.robot.nerdyfiles.vision.LimelightUtilities;
 
-import java.nio.channels.Pipe;
-import java.util.function.Supplier;
-
 import org.littletonrobotics.junction.Logger;
-
 
 public class Vision extends SubsystemBase {
 
