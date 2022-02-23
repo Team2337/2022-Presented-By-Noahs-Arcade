e0fcf3bca3ac89ff039cad334256c8d3eae13351
Nicholas-Stokes <58690071+Nicholas-Stokes@users.noreply.github.com>
2022-02-23 11:36:41 -0500

    Changed Logic for if we have an error code (#144)
    * Changed Logic for if we have an error code
    
    If we have a non-standard status from our ToF sensor, we can return an error code of -1
    
    * Added counter for Invalids
    
    Cycles subject to change. If over threshold for invalids (or any other error code) in a row, we get a -1.
    
    * Code Review Changes
    
    * Updated Counter
    
    * Small cleanup. Fix typo
    
    Co-authored-by: Zachary Orr <516458+ZachOrr@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/hardware/TimeOfFlightSensor.java
+++ src/main/java/frc/robot/subsystems/hardware/TimeOfFlightSensor.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.hardware;
 import com.playingwithfusion.TimeOfFlight;
 import com.playingwithfusion.TimeOfFlight.RangingMode;
 import com.playingwithfusion.TimeOfFlight.Status;
-
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,6 +12,8 @@ public class TimeOfFlightSensor extends SubsystemBase {
   private final TimeOfFlight sensor = new TimeOfFlight(0);
 
   private double distanceInches;
+  private int validCounter = 0;
+  private final int MAX_VALID_COUNTER = 3;
 
   public TimeOfFlightSensor() {
     sensor.setRangingMode(RangingMode.Short, 24);
@@ -22,8 +23,15 @@ public class TimeOfFlightSensor extends SubsystemBase {
   @Override
   public void periodic() {
     if (getStatus() == Status.Valid) {
+      validCounter = 0;
       // mm -> m, then m -> inches
       distanceInches = Units.metersToInches((getDistanceMM() / 1000));
+    } else {
+      validCounter++;
+      if (validCounter >= MAX_VALID_COUNTER) {
+        distanceInches = -1;
+        validCounter = 0;
+      }
     }
 
     SmartDashboard.putNumber("TimeOfFlight/Distance (in)", getDistanceInches());
