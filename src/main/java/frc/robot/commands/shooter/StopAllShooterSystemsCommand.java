10364d663a82c22f712d48cf1da6996c0e75f40f
Nicholas-Stokes <plexagongb@gmail.com>
2022-03-07 17:44:34 -0500

    Small Changes

--- src/main/java/frc/robot/commands/shooter/StopAllShooterSystemsCommand.java
+++ src/main/java/frc/robot/commands/shooter/StopAllShooterSystemsCommand.java
@@ -2,7 +2,6 @@ package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import frc.robot.commands.delivery.AutoStopDelivery;
 import frc.robot.subsystems.Delivery;
 import frc.robot.subsystems.Kicker;
 import frc.robot.subsystems.Shooter;
@@ -11,9 +10,9 @@ public class StopAllShooterSystemsCommand extends ParallelCommandGroup {
 
   public StopAllShooterSystemsCommand(Delivery delivery, Kicker kicker, Shooter shooter) {
     addCommands(
-      new AutoStopDelivery(delivery),
-      new AutoStopShooter(shooter),
-      new AutoStopKicker(kicker)
+      new InstantCommand(delivery::stopDelivery, delivery),
+      new InstantCommand(shooter::stop, shooter),
+      new InstantCommand(kicker::stop, kicker)
     );
   }
 
