4e4be53ab82d953e6de2524b3dbe7f0008f90356
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-03-10 00:09:17 -0500

    Convert ToF sensor to not be a subsystem (#196)

--- src/main/java/frc/robot/subsystems/hardware/TimeOfFlightSensor.java
+++ src/main/java/frc/robot/subsystems/hardware/TimeOfFlightSensor.java
@@ -2,46 +2,27 @@ package frc.robot.subsystems.hardware;
 
 import com.playingwithfusion.TimeOfFlight;
 import com.playingwithfusion.TimeOfFlight.RangingMode;
-import com.playingwithfusion.TimeOfFlight.Status;
 
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class TimeOfFlightSensor extends SubsystemBase {
+public class TimeOfFlightSensor {
 
   private final TimeOfFlight sensor = new TimeOfFlight(0);
-
-  private double distanceInches;
 
   public TimeOfFlightSensor() {
     sensor.setRangingMode(RangingMode.Short, 24);
     sensor.setRangeOfInterest(6, 6, 10, 10);
   }
 
-  @Override
-  public void periodic() {
-    distanceInches = Units.metersToInches((getDistanceMM() / 1000));
-
-    SmartDashboard.putNumber("TimeOfFlight/Distance (in)", getDistanceInches());
-    SmartDashboard.putString("TimeOfFlight/Status", getStatus().toString());
-  }
-
-  private Status getStatus() {
-    return sensor.getStatus();
+  /**
+   * Get the last read distance from the sensor in inches.
+   */
+  public double getDistanceInches() {
+    return Units.metersToInches((getDistanceMM() / 1000));
   }
 
   private double getDistanceMM() {
     return sensor.getRange();
-  }
-
-  /**
-   * Get the last read distance from the sensor in inches.
-   * Note: If the sensor's status is no longer valid, we will
-   * remember it's last returned distance.
-   */
-  public double getDistanceInches() {
-    return distanceInches;
   }
 
 }
