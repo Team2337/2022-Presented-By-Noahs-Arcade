049c560f8945844cc0d493fa234da4440a295719
slynch05 <slynch05@yahoo.com>
2022-03-14 18:23:49 -0400

    Removed driver station from heading

--- src/main/java/frc/robot/subsystems/Heading.java
+++ src/main/java/frc/robot/subsystems/Heading.java
@@ -5,7 +5,6 @@ import java.util.function.Supplier;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.nerdyfiles.utilities.Utilities;
