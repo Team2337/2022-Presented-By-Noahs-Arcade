dae51749f56753ff73325288ccae691fee2e60d2
slynch05 <slynch05@yahoo.com>
2022-03-21 19:51:46 -0400

    Livonia comp (#218)
    * Changes from livonia comp
    
    * Added new shooter commands
    
    * Uncommented climber logic

--- src/main/java/frc/robot/commands/shooter/PrepareShooter.java
+++ src/main/java/frc/robot/commands/shooter/PrepareShooter.java
@@ -5,25 +5,25 @@ import java.util.function.Supplier;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.delivery.StartDelivery;
-import frc.robot.commands.kicker.ForwardKickerCommand;
 import frc.robot.commands.kicker.ReverseKickerCommand;
-import frc.robot.commands.kicker.StartKicker;
-import frc.robot.subsystems.Delivery;
+import frc.robot.commands.swerve.EnableMaintainHeading;
+import frc.robot.commands.vision.LimeLightHeadingCommand;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Vision;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Heading;
 import frc.robot.subsystems.Kicker;
 
-public class LinearShootCommand extends SequentialCommandGroup {
+public class PrepareShooter extends SequentialCommandGroup {
 
-  public LinearShootCommand(Supplier<Translation2d> translationSupplier, Supplier<Boolean> overrideSupplier, Delivery delivery, Kicker kicker, Shooter shooter) {
+  public PrepareShooter(Supplier<Translation2d> translationSupplier, Supplier<Boolean> overrideSupplier, Drivetrain drivetrain, Heading heading, Kicker kicker, Shooter shooter, Vision vision) {
     addCommands(
       new ReverseStopShooterCommand(shooter).withTimeout(0.2),
       new ReverseKickerCommand(kicker).withTimeout(0.2),
       new WaitCommand(0.4),
       new StartShooterUpToSpeedDistanceCommand(translationSupplier,  overrideSupplier, shooter),
-      new StartKicker(kicker),
-      new StartDelivery(delivery)
+      new EnableMaintainHeading(heading),
+      new LimeLightHeadingCommand(drivetrain, heading, vision)
     );
   }
-
 }
