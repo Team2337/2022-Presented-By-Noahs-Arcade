3e44e2f893def0602d2068e69486f26db5096c9a
slynch05 <slynch05@yahoo.com>
2022-04-10 17:47:16 -0400

    testing

--- src/main/java/frc/robot/commands/auto/Pos2MidFiveBall.java
+++ src/main/java/frc/robot/commands/auto/Pos2MidFiveBall.java
@@ -9,6 +9,7 @@ import frc.robot.commands.auto.commandGroups.AutoStopAllCommands;
 import frc.robot.commands.auto.commandGroups.FirstMove;
 import frc.robot.commands.delivery.AutoStartDelivery;
 import frc.robot.commands.delivery.AutoStartDeliveryLeft;
+import frc.robot.commands.intake.AutoStartIntake;
 import frc.robot.commands.kicker.ForwardKickerCommand;
 import frc.robot.commands.shooter.StartShooterInstantCommand;
 import frc.robot.subsystems.AutoDrive;
@@ -20,15 +21,23 @@ import frc.robot.subsystems.Kicker;
 import frc.robot.subsystems.Shooter;
 
 public class Pos2MidFiveBall extends SequentialCommandGroup {
+  private double forwardP = 1.5;
+  private double strafeP = 0.05;
+  private double forwardAcceleration = Units.inchesToMeters(45);
+  private double strafeAcceleration = 8;
 
   public Pos2MidFiveBall(AutoDrive autoDrive, Delivery delivery, Drivetrain drivetrain, Heading heading, Intake intake, Kicker kicker, Shooter shooter) {
     addCommands(
-      new FirstMove(Constants.Auto.kBallR2ShootPosition, autoDrive, drivetrain, heading, intake, shooter),
+      new StartShooterInstantCommand(42, shooter),
+      new ParallelCommandGroup(
+        new AutoStartIntake(intake),
+        new ProfiledPointToPointCommand(Constants.Auto.kBallR2ShootPosition, drivetrain::getTranslation, intake::getAutoBeamBreakSensorStatus, forwardP, strafeP, forwardAcceleration, strafeAcceleration, true, autoDrive, heading).withTimeout(2)),
       new ParallelCommandGroup(
         new ForwardKickerCommand(kicker).withTimeout(0.75),
         new AutoStartDeliveryLeft(delivery).withTimeout(0.75)
       ),
       new ProfiledPointToPointCommand(Constants.Auto.TransitionBetweenBallR2AndBallR4, drivetrain::getTranslation, intake::getAutoBeamBreakSensorStatus,1.5, 0.05, Units.inchesToMeters(60), 8, true, autoDrive, heading).withTimeout(2.5),
+      /*
       new ProfiledPointToPointCommand(Constants.Auto.kBallR4Pickup, drivetrain::getTranslation, intake::getAutoBeamBreakSensorStatus, 1.5, 0.05, Units.inchesToMeters(60), 8, true, autoDrive, heading).withTimeout(1.5),
       new StartShooterInstantCommand(43, shooter),
       new ParallelCommandGroup(
@@ -48,6 +57,7 @@ public class Pos2MidFiveBall extends SequentialCommandGroup {
         new AutoStartDelivery(delivery).withTimeout(1)
       ),
       new WaitCommand(2),
+      */
       // new ProfiledPointToPointCommand(Constants.Auto.kPosition3RightStart, drivetrain::getTranslation, 1.0, 0.05, Units.inchesToMeters(120), 8, autoDrive, heading).withTimeout(3),
       new AutoStopAllCommands(delivery, intake, kicker, shooter)
     );
